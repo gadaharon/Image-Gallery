34fc5582e0c7f81e6ad2738127b65aae835d393b
gad-restart <gada@restartit.co.il>
2019-09-06 19:37:15 +0300

    initial commit

---
+++ ImageGalley-Server/ImageGallery/Controllers/ImagesController.cs
@@ -0,0 +1,41 @@
+﻿using ImageGallery.BAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ImageGallery.Controllers
+{
+    public class ImagesController : ApiController
+    {
+        // GET: api/Images
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Images/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Images
+        public Image Post([FromBody]Image image)
+        {
+			return image; 
+        }
+
+        // PUT: api/Images/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Images/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
